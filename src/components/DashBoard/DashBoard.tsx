89912ee680dbc7d521165ba81611e8bf817975b3
AisinAllin <AisinAllin@gmail.com>
2021-03-08 22:35:22 +1100

    commit

--- src/components/DashBoard/DashBoard.tsx
+++ src/components/DashBoard/DashBoard.tsx
@@ -4,12 +4,6 @@ import { connect } from 'react-redux';
 import NavBar from '../NavBar/NavBar';
 import * as actionCreators from '../../stroe/actions/index';
 import LoginTest from './LoginTest';
-// import {
-//     BrowserRouter as Router,
-//     Switch,
-//     Route,
-//     Link
-// } from "react-router-dom";
 
 const Dashboard = (props:any) => {
   console.log('Dashboard', props.value);
